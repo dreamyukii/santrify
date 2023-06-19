df6387ac96ddced6b52ef408605833ed4142a664
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-06-20 01:14:23 +0700

    feat: add card room list

--- src/app/room/page.js
+++ src/app/room/page.js
@@ -1,5 +1,66 @@
+import Image from "next/image";
 export default function Room() {
   return (
-    <div>Room</div>
-  )
+    <>
+      <div className="container d-flex">
+        <div className="row justify-content-start">
+          <div className="col">
+            <div
+              className="card text-bg-dark"
+              style={{ width: 200, height: 200 }}
+            >
+              <Image width={200} height={200} src="/santri_login.jpg" />
+              <div className="card-img-overay">
+                <h5 className="card-title">Kamar Jeruk</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="card text-bg-dark"
+              style={{ width: 200, height: 200 }}
+            >
+              <Image width={200} height={200} src="/santri_login.jpg" />
+              <div className="card-img-overay">
+                <h5 className="card-title">Kamar Jeruk</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="card text-bg-dark"
+              style={{ width: 200, height: 200 }}
+            >
+              <Image width={200} height={200} src="/santri_login.jpg" />
+              <div className="card-img-overay">
+                <h5 className="card-title">Kamar Jeruk</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="card text-bg-dark"
+              style={{ width: 200, height: 200 }}
+            >
+              <Image width={200} height={200} src="/santri_login.jpg" />
+              <div className="card-img-overay">
+                <h5 className="card-title">Kamar Jeruk</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              className="card text-bg-dark"
+              style={{ width: 200, height: 200 }}
+            >
+              <Image width={200} height={200} src="/santri_login.jpg" />
+              <div className="card-img-overay">
+                <h5 className="card-title">Kamar Jeruk</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
