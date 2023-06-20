f2acc61df05056462b4eadff590c37620310e45e
RFaizal <faizalramadhandi@amikom.ac.id>
2023-06-20 07:32:39 +0700

    remake login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,70 +6,85 @@ import "./login.css";
 export default class login extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-6 ">
-          <div className="card login-page">
-            <div className="card-body px-1">
-              {/* Login Page */}
-              <div className="">
-                <div className="row h-100 justify-content-center align-items-center">
-                  <div className="">
-                    <div className="header">
-                      <h1 className="">Welcome To Santrify</h1>
-                      <div className="login-form">
-                        <label htmlFor="email" className="form-label">
-                          Username
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control "
-                          id="email"
-                          placeholder="Username"
-                        />
-                        <label htmlFor="password" className="form-label">
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          className="form-control "
-                          id="password"
-                          placeholder="Password"
-                        />
-                        <a
-                          href="#"
-                          className="text-decoration-none  my-2 text-left d-block "
-                        >
-                          Forgot Password ?
-                        </a>
-                        <a href="../index.html">
-                          <button className="btn btn-success text-center align-items-center ">
-                            Login
-                          </button>
-                        </a>
-                        <div className="my-2">
-                          <span className="">Don't Have an Account ? </span>
-                          <a href="#" className="text-decoration-none fw-bold">
-                            Sign up now
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+		<div class="container-fluid ps-md-0">
+			<div class="row g-0">
+				<div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+				<div class="col-md-8 col-lg-6">
+					<div class="login d-flex align-items-center ">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-9 col-lg-8 mx-auto">
+									<div className="header text-center">
+										<h1 class="login-heading mb-0 ">
+											Log In
+										</h1>
+										<p className="small ">
+											Enter Your Details to Continue
+										</p>
+									</div>
 
-        <div className="col-2">
-          <Image
-            src="/santri_login.jpg"
-            width={1000}
-            height={945}
-            alt="santri"
-          />
-        </div>
-      </div>
-    );
+									<form>
+										<div class="form-floating ">
+											<input
+												type="email"
+												className="form-control"
+												id="floatingInput"
+												placeholder="Email Address/Username"
+											/>
+											<label for="floatingInput">
+												Email address / Username
+											</label>
+										</div>
+										<div class="form-floating mb-3">
+											<input
+												type="password"
+												className="form-control"
+												id="floatingPassword"
+												placeholder="Password"
+											/>
+											<label for="floatingPassword">
+												Password
+											</label>
+										</div>
+
+										<div class="form-check mb-3">
+											<input
+												className="form-check-input"
+												type="checkbox"
+												value=""
+												id="rememberPasswordCheck"
+											/>
+											<label
+												className="form-check-label"
+												for="rememberPasswordCheck"
+											>
+												Remember password
+											</label>
+										</div>
+										<div className=" d-block">
+											<a
+												className="small text-decoration-none"
+												href="#"
+											>
+												Forgot password?
+											</a>
+										</div>
+
+										<div class="d-grid mt-4">
+											<a href="../index.html">
+												<button className="btn btn-success text-center align-items-center ">
+													Login
+												</button>
+											</a>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
   }
 }
