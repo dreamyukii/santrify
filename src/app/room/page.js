063365f28fea32cc2a523e42627baea4d370b513
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-06-21 15:37:45 +0700

    fix: room

--- src/app/room/page.js
+++ src/app/room/page.js
@@ -3,15 +3,20 @@ export default function Room() {
   return (
     <>
       <div className="container d-flex">
-        <div className="row justify-content-start">
+        <div className="row justify-items-center ">
           <div className="col">
             <div
-              className="card text-bg-dark"
-              style={{ width: 200, height: 200 }}
+              className="card border-primary"
+              style={{ width: 200, height: 200}}
             >
-              <Image width={200} height={200} src="/santri_login.jpg" />
-              <div className="card-img-overay">
-                <h5 className="card-title">Kamar Jeruk</h5>
+              <Image width={200} height={200} src="/kamar_putra.jpg"/>
+              <div className="card-img-overlay">
+                <h5 className="card-title">Kamar Yastrib</h5>
+                <br />
+                <br />
+                <br />
+                <br />
+                <button className="btn btn-primary">Show</button>
               </div>
             </div>
           </div>
@@ -21,8 +26,13 @@ export default function Room() {
               style={{ width: 200, height: 200 }}
             >
               <Image width={200} height={200} src="/santri_login.jpg" />
-              <div className="card-img-overay">
+              <div className="card-img-overlay">
                 <h5 className="card-title">Kamar Jeruk</h5>
+                <br />
+                <br />
+                <br />
+                <br />
+                <button className="btn btn-primary">Show</button>
               </div>
             </div>
           </div>
@@ -32,30 +42,13 @@ export default function Room() {
               style={{ width: 200, height: 200 }}
             >
               <Image width={200} height={200} src="/santri_login.jpg" />
-              <div className="card-img-overay">
+              <div className="card-img-overlay">
                 <h5 className="card-title">Kamar Jeruk</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div
-              className="card text-bg-dark"
-              style={{ width: 200, height: 200 }}
-            >
-              <Image width={200} height={200} src="/santri_login.jpg" />
-              <div className="card-img-overay">
-                <h5 className="card-title">Kamar Jeruk</h5>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div
-              className="card text-bg-dark"
-              style={{ width: 200, height: 200 }}
-            >
-              <Image width={200} height={200} src="/santri_login.jpg" />
-              <div className="card-img-overay">
-                <h5 className="card-title">Kamar Jeruk</h5>
+                <br />
+                <br />
+                <br />
+                <br />
+                <button className="btn btn-primary">Show</button>
               </div>
             </div>
           </div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -8,7 +8,12 @@ export default class login extends Component {
     return (
 		<div class="container-fluid ps-md-0">
 			<div class="row g-0">
-				<div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+				<div class="d-none d-md-flex col-md-4 col-lg-6 bg-image">
+					<Image
+					src="/santri_login.jpg"
+					width="750"
+					height="800"></Image>
+				</div>
 				<div class="col-md-8 col-lg-6">
 					<div class="login d-flex align-items-center ">
 						<div class="container">
