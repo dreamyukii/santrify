180bd20c1307cd66e6a8387c35bb9b87b6d30c0d
RFaizal <faizalramadhandi@amikom.ac.id>
2023-07-05 13:07:43 +0700

    adding insight & remake table

--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,13 @@ export default function Page() {
       <div>
         <div className="container">
           <div className="row">
-            <div className="col-4">
+            <div className="col-3">
               <InformasiDana />
             </div>
-            <div className="col-4">
+            <div className="col-3">
               <JumlahSantri/>
             </div>
-            <div className="col-4">
+            <div className="col-3">
               <JumlahKamar/>
             </div>
             <div className="col-12"></div>
