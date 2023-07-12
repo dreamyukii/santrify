689a31d5ea821ee00305b9e54ba84ff8d4877f9d
Primasbt <primasbt@gmail.com>
2023-07-12 23:22:21 +0700

    AMBASSINGGG

--- src/app/dashboard/history/page.js
+++ src/app/dashboard/history/page.js
@@ -1,9 +1,42 @@
 export default function History() {
   return (
-    <div>
-      <table class="table table-striped table-hover">
-  ...
-</table>
-    </div>
+    <TableComponent/>    
   )
+
+  function TableComponent() {
+    const data = [
+      { id: 1, aksi: 'John',tanggal: '19/07/2023' },
+      { id: 2, aksi: 'John',tanggal: '19/07/2023' },
+      { id: 3, aksi: 'John',tanggal: '19/07/2023' },
+      { id: 4, aksi: 'John',tanggal: '19/07/2023' },
+      // Add more data objects as needed
+    ];
+  
+    return (
+      <table className="table ">
+  <thead>
+    <tr className="table-success">
+      <th scope="col">No.</th>
+      <th scope="col">Aksi</th>
+      <th scope="col">Tanggal</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      data.map((val)=>{
+       return (
+      
+         <tr>
+          <td scope="row">{val.id}</td>
+          <td>{val.aksi}</td>
+          <td>{val.tanggal}</td>
+      </tr>
+          )
+      })
+    }
+  </tbody>
+</table>
+    );
+  }
+  
 }
